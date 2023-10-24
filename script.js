a01fee92b3fe6ea8bc2bd90c34221f3daf3f299e
SEO-Butler <146767110+SEO-Butler@users.noreply.github.com>
2023-10-24 14:13:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 const params = new URLSearchParams(window.location.search);
 
 const name = params.get('name');
-const score = params.get('score');
 const imageUrl = params.get('image');
 
 document.getElementById('name').textContent = name || 'Unknown';
-document.getElementById('score').textContent = score || '0';
 document.getElementById('image').src = imageUrl || 'default-image.jpg';
